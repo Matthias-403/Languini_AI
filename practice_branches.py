5b127812fee6a329a58f6bec12ddd1d458b29120
Jonathan Sarzosa <jonathan.sarzosa1996@gmail.com>
2022-11-28 12:59:21 +0000

    Practice branches

--- practice_branches.py
+++ practice_branches.py
@@ -5,7 +5,8 @@ def n_function():
   print("Hello from a function")
 
 def j_function():
-  print("Hello from a function")
+    sum = 1 + 2
+    return sum
 
 def m_function():
   print("Hello from a function")
