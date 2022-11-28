3e30206e900aa5e3afefb11dcf5ae11689fac99d
Maciej Psiuch <maciejowiczan@gmail.com>
2022-11-28 12:18:53 +0000

    testing contributions from collaborators and GitHub set up and priviledges

---
+++ practice_branches.py
@@ -0,0 +1,11 @@
+def z_function():
+  print("Hello from a function")
+
+def n_function():
+  print("Hello from a function")
+
+def j_function():
+  print("Hello from a function")
+
+def m_function():
+  print("Hello from a function")
