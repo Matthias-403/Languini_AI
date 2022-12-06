64cee4268c06e57af70d0da7676583e7763d823b
Zachary Ma <zacharyma.info@gmail.com>
2022-12-06 12:26:11 +0000

    re-structure repo, all .py file in 1 directory

--- languiniai/data_preprocessing/metrics.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import librosa
-import math
-
-def rms(wav: np.ndarray) -> np.ndarray:
-    '''calculate audio rms'''
-    return math.sqrt( np.mean( wav**2 ) )
--- languiniai/data_preprocessing/utils.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import librosa
-import os
-
-def load_wavs(wavs: np.ndarray) -> np.ndarray:
-    return np.ndarray( list(map( lambda file: librosa.load(f'{dir}/{file}'), os.listdir(dir) )) )
-
